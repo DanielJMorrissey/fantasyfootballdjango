73c534e002c330fe5152eb5e25b5568ce4d0d757
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-09-07 10:18:30 +0100

    created database tables

--- loginregistration/models.py
+++ loginregistration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserLoginReg(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    signin = models.BooleanField(default=False)
+    password = models.CharField(max_length=50)
+    funds = models.IntegerField(default=100000000)
+    score = models.IntegerField(default=0)
+    teamname = models.CharField(max_length=255)
--- players/models.py
+++ players/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class PlayerList(models.Model):
+    userid = models.IntegerField()
+    firstname = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    nationality = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    value = models.IntegerField()
+    
