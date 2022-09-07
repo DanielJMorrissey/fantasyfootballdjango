e441c7e614803e1a88001fe2422f381b2962b38a
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-09-07 11:43:34 +0100

    adjusted nav to show different links depending on log in status

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from loginregistration.models import UserLoginReg
+
+"""
+  need to sort out for log in and reg:
+    create session
+    remove record
+    make only a single user can be logged in at a time
+    each template gets the sign in boolean
+    each view checks if logged in
+    if logged in or registered, log in and reg page won't show
+"""
 
 # Create your views here.
 def home(request):
+  userLoggedIn = UserLoginReg.objects.all().values()
   template = loader.get_template('homepage.html')
-  return HttpResponse(template.render())
+  context= {
+    'user' : userLoggedIn,
+  }
+  return HttpResponse(template.render(context, request))
