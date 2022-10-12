70be0dceeb612998b3d16c13110d5add74b741aa
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-10-12 13:23:31 +0100

    updated players model

--- players/models.py
+++ players/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class PlayerList(models.Model):
-    userid = models.IntegerField()
+    userid = models.IntegerField(null=True)
     firstname = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     nationality = models.CharField(max_length=255)
